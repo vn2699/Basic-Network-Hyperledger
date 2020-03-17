533700ffe3f554aeab6e678bf014f6d40e514467
varun <vn2699@gmail.com>
2020-03-17 18:13:43 +0530

    seventh commit

--- scripts/env-change.sh
+++ scripts/env-change.sh
@@ -10,13 +10,38 @@ fi
 # export CORE_PEER_ID=peer0.$ORG_NAME2.com
 
 # export ORG_NAME2=acme
-export CORE_PEER_CHAINCODELISTENADDRESS=peer0.$ORG_NAME2.com:7052
-export CORE_PEER_ADDRESS=peer0.$ORG_NAME2.com:7051
-export CORE_PEER_TLS_ENABLED=false
-export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/$ORG_NAME2.com/users/Admin@$ORG_NAME2.com/msp
-export ORG_NAME1=${ORG_NAME2^}
-# echo $ORG_NAME1
-export CORE_PEER_LOCALMSPID="${ORG_NAME1}MSP"
+case ORG_NAME2 in
+    "acme" )
+        export CORE_PEER_CHAINCODELISTENADDRESS=peer0.$ORG_NAME2.com:7052
+        export CORE_PEER_ADDRESS=peer0.$ORG_NAME2.com:7051
+        export CORE_PEER_TLS_ENABLED=false
+        export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/$ORG_NAME2.com/users/Admin@$ORG_NAME2.com/msp
+        export ORG_NAME1=${ORG_NAME2^}
+        # echo $ORG_NAME1
+        export CORE_PEER_LOCALMSPID="${ORG_NAME1}MSP"
+        ;;
+    "budget" )
+        export CORE_PEER_CHAINCODELISTENADDRESS=peer0.$ORG_NAME2.com:8052
+        export CORE_PEER_ADDRESS=peer0.$ORG_NAME2.com:8051
+        export CORE_PEER_TLS_ENABLED=false
+        export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/$ORG_NAME2.com/users/Admin@$ORG_NAME2.com/msp
+        export ORG_NAME1=${ORG_NAME2^}
+        # echo $ORG_NAME1
+        export CORE_PEER_LOCALMSPID="${ORG_NAME1}MSP"
+        ;;
+    "expo" )
+        export CORE_PEER_CHAINCODELISTENADDRESS=peer0.$ORG_NAME2.com:9052
+        export CORE_PEER_ADDRESS=peer0.$ORG_NAME2.com:9051
+        export CORE_PEER_TLS_ENABLED=false
+        export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/$ORG_NAME2.com/users/Admin@$ORG_NAME2.com/msp
+        export ORG_NAME1=${ORG_NAME2^}
+        # echo $ORG_NAME1
+        export CORE_PEER_LOCALMSPID="${ORG_NAME1}MSP"
+        ;;
+    * )
+        echo "Invalid org name"
+        ;;
+esac
 # export ORG_NAME=peer0.$ORG_NAME2.com    
 # export ORDERER_ADDRESS=orderer.acme.com:7050
 # export ORG_NAME2=budget
