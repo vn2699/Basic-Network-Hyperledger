17494e820048ea7ae480f470ed926bbc916105b4
varun <vn2699@gmail.com>
2020-03-17 16:19:55 +0530

    fourth commit

--- scripts/env-change.sh
+++ scripts/env-change.sh
@@ -9,7 +9,7 @@ else
 fi
 # export CORE_PEER_ID=peer0.$ORG_NAME2.com
 
-export ORG_NAME2=acme
+# export ORG_NAME2=acme
 export CORE_PEER_CHAINCODELISTENADDRESS=peer0.$ORG_NAME2.com:7052
 export CORE_PEER_ADDRESS=peer0.$ORG_NAME2.com:7051
 export CORE_PEER_TLS_ENABLED=false
@@ -19,13 +19,13 @@ export ORG_NAME1=${ORG_NAME2^}
 export CORE_PEER_LOCALMSPID="${ORG_NAME1}MSP"
 # export ORG_NAME=peer0.$ORG_NAME2.com    
 # export ORDERER_ADDRESS=orderer.acme.com:7050
-export ORG_NAME2=budget
-export CORE_PEER_CHAINCODELISTENADDRESS=peer0.$ORG_NAME2.com:8052
-export CORE_PEER_ADDRESS=peer0.$ORG_NAME2.com:8051
-export CORE_PEER_TLS_ENABLED=false
-export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/$ORG_NAME2.com/users/Admin@$ORG_NAME2.com/msp
-export ORG_NAME1=${ORG_NAME2^}
-# echo $ORG_NAME1
-export CORE_PEER_LOCALMSPID="${ORG_NAME1}MSP"
+# export ORG_NAME2=budget
+# export CORE_PEER_CHAINCODELISTENADDRESS=peer0.$ORG_NAME2.com:8052
+# export CORE_PEER_ADDRESS=peer0.$ORG_NAME2.com:8051
+# export CORE_PEER_TLS_ENABLED=false
+# export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/$ORG_NAME2.com/users/Admin@$ORG_NAME2.com/msp
+# export ORG_NAME1=${ORG_NAME2^}
+# # echo $ORG_NAME1
+# export CORE_PEER_LOCALMSPID="${ORG_NAME1}MSP"
 echo -e "==========>ENV VARIABLES<==============\n"
 env | grep CORE_
--- scripts/peer-update.sh
+++ scripts/peer-update.sh
@@ -14,4 +14,4 @@ peer channel signconfigtx -f ./network-config/AcmeAnchor.tx
 
 # configtxgen -outputAnchorPeersUpdate /opt/gopath/src/github.com/hyperledger/fabric/peer/network-config -channelID airlinechannel -asOrg $ORG_NAME -profile AirlineChannel
 
-peer channel update -f /opt/gopath/src/github.com/hyperledger/fabric/peer/network-config/AcmeAnchor.tx -c airlinechannel -o orderer.acme.com:7050
+peer channel update -f /opt/gopath/src/github.com/hyperledger/fabric/peer/network-config/AcmeAnchor.tx -c airlinechannel -o orderer0.acme.com:7050
